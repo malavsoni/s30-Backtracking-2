385cf7ec5c37224ec5ec0ea599e913bec405ae83
Malav Soni <malav@live.com>
2024-11-28 22:18:07 +0530

    subset problem done

---
+++ subsets.ts
@@ -0,0 +1,60 @@
+function subsets(nums: number[]): number[][] {
+  let result: number[][] = [];
+
+  function backtracking(nums: number[], index: number, path: number[]) {
+    // Base
+    if (index == nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    // Logic
+    // No Choose
+    backtracking(nums, index + 1, path);
+
+    // Choose
+    path.push(nums[index]);
+    backtracking(nums, index + 1, path);
+    path.pop();
+  }
+
+  backtracking(nums, 0, []);
+
+  return result;
+}
+
+function subsets_with_pivot(nums: number[]): number[][] {
+  let result: number[][] = [];
+
+  function backtracking(nums: number[], pivot: number, path: number[]) {
+    // Base
+    result.push([...path]);
+
+    // Logic
+    for (let index = pivot; index < nums.length; index++) {
+      path.push(nums[index]);
+      backtracking(nums, index + 1, path);
+      path.pop();
+    }
+  }
+
+  backtracking(nums, 0, []);
+
+  return result;
+}
+
+describe("Subsets", () => {
+  it("Happy Path - 01", () => {
+    let candidates = [1, 2, 3];
+    expect(subsets_with_pivot(candidates)).toEqual([
+      [],
+      [1],
+      [2],
+      [1, 2],
+      [3],
+      [1, 3],
+      [2, 3],
+      [1, 2, 3],
+    ]);
+  });
+});
